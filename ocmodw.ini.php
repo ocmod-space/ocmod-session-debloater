251ff23aa507e911aa2c74e312fad6e6f28cb227
akaunderr <akaunderr@gmail.com>
2024-06-12 07:44:04 +0300

    updated module wrapper

---
+++ ocmodw.ini.php
@@ -0,0 +1,14 @@
+<?php
+
+define('VERSION', '1.2.1');
+define('YEAR', '2020');
+
+define('AUTHOR', 'Andrii Burkatskyi');
+define('EMAIL', 'ocmod.space@gmail.com');
+
+define('MODLINK', 'https://www.opencart.com/index.php?route=marketplace/extension/info&extension_id=38580');
+define('OCMLINK', 'https://www.opencart.com/index.php?route=marketplace/extension/info&extension_id=38580');
+define('GITLINK', 'https://github.com/ocmod-space/ocmod-session-debloater');
+define('XMLLINK', 'https://www.opencart.com/index.php?route=marketplace/extension&amp;filter_member=ocmod.space');
+
+define('LICENSE', 'https://raw.githubusercontent.com/ocmod-space/ocmod-session-debloater/main/LICENSE.txt');
